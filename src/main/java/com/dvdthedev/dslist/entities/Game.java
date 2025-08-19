9be8f77046f27bc552f1a8737e5c9c99bd4c5bcb
dvdthedev <dvd2.0@live.com>
2025-08-19 10:47:19 -0300

    game seed

--- src/main/java/com/dvdthedev/dslist/entities/Game.java
+++ src/main/java/com/dvdthedev/dslist/entities/Game.java
@@ -15,20 +15,24 @@ public class Game {
     private Integer year;
     private String genre;
     private String platform;
+    private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game() {
     }
 
-    public Game(Long id, String title, Integer year, String genre, String platform, String imgUrl, String shortDescription,
+    public Game(Long id, String title, Integer year, String genre, String platform,Double score, String imgUrl, String shortDescription,
                 String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
         this.platform = platform;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
@@ -108,5 +112,13 @@ public class Game {
 
     public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
+    }
+
+    public Double getScore() {
+        return score;
+    }
+
+    public void setScore(Double score) {
+        this.score = score;
     }
 }
